0581702472e77a44bec88286f3430ea8a41fd2a6
yu <yul1024@outlook.com>
2025-05-29 15:09:27 +0800

    Create test_prompt_template_loader.py

---
+++ tests/test_langchain_toolkit/test_prompt_management/test_prompt_template_loader.py
@@ -0,0 +1,14 @@
+"""
+测试PromptTemplateLoader的功能。
+"""
+
+import pytest
+
+from langchain_toolkit.prompt_management.prompt_template_loader import PromptTemplateLoader
+
+
+class TestPromptTemplateLoader:
+    @pytest.mark.parametrize('inputs, expected', )
+    def test_load_prompt_template(self, inputs, expected):
+        ...
+
