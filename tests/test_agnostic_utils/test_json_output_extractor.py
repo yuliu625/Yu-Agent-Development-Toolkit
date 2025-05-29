b848aeaff8a7c9a183937bd7e39cb6352a935e7f
yu <yul1024@outlook.com>
2025-05-29 15:05:33 +0800

    Create test_json_output_extractor.py

---
+++ tests/test_agnostic_utils/test_json_output_extractor.py
@@ -0,0 +1,30 @@
+"""
+测试JsonOutputExtractor功能。
+
+这是一个重要的工具类，未来任需要额外扩展。
+"""
+
+import pytest
+
+from agnostic_utils.json_output_extractor import JsonOutputExtractor
+
+_test_extract_json_from_str_cases = [
+    (
+        dict(
+            raw_str='```json{"a": 1, "b": 2}```',
+            index_to_choose=-1,
+            json_loader_name='json-repair',
+            schema_pydantic_base_model=None,
+            schema_check_type='dict',
+        ),
+        {'a': 1, 'b': 2},
+    ),
+]
+
+
+class TestJsonOutputExtractor:
+    @pytest.mark.parametrize('inputs, expected', _test_extract_json_from_str_cases)
+    def test_extract_json_from_str(self, inputs, expected):
+        json_data = JsonOutputExtractor.extract_json_from_str(**inputs)
+        assert json_data == expected
+
