8a0b14d390417347f2191fd973b76be65e5d86ee
yu <yul1024@outlook.com>
2025-05-29 15:06:02 +0800

    Create test_json_input_processor.py

---
+++ tests/test_agnostic_utils/test_json_input_processor.py
@@ -0,0 +1,30 @@
+"""
+测试JsonInputProcessor的运行情况。
+"""
+
+import pytest
+
+from agnostic_utils.json_input_processor import JsonInputProcessor
+
+_test_put_in_markdown_cases = [
+    ([1, 2, 3], "```json\n[1, 2, 3]\n```"),
+    ({'a': 1, 'b': 2}, '```json\n{"a": 1, "b": 2}\n```'),
+]
+
+_test_get_json_str_from_python_structured_data_cases = [
+    ([1, 2, 3], "[1, 2, 3]"),
+    ({'a': 1, 'b': 2}, '{"a": 1, "b": 2}'),
+]
+
+
+class TestJsonInputProcessor:
+    @pytest.mark.parametrize('inputs, expected', _test_put_in_markdown_cases)
+    def test_put_in_markdown(self, inputs, expected):
+        markdown_str = JsonInputProcessor.put_in_markdown(inputs)
+        assert markdown_str == expected
+
+    @pytest.mark.parametrize('inputs, expected', _test_get_json_str_from_python_structured_data_cases)
+    def test_get_json_str_from_python_structured_data(self, inputs, expected):
+        json_str = JsonInputProcessor.get_json_str_from_python_structured_data(inputs)
+        assert json_str == expected
+
