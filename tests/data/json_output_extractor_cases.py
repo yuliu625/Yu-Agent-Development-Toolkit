a7c6f26cb23a50332ea4e89613ee82ffd3a01dbb
yu <yul1024@outlook.com>
2025-05-30 19:19:01 +0800

    Create json_output_extractor_cases.py

---
+++ tests/data/json_output_extractor_cases.py
@@ -0,0 +1,54 @@
+"""
+JsonOutputExtractor的测试用例。
+
+这个测试很复杂，因此数据数据构建是通过py文件。
+"""
+
+from pydantic import BaseModel, Field
+
+
+class _TestListDataClass(BaseModel):
+    items: list[str] = Field(...)
+
+
+class _TestDictDataClass(BaseModel):
+    a: int
+    b: str
+    c: bool
+
+
+JSON_OUTPUT_EXTRACTOR_CASES = [
+    # dict-cases
+    (
+        dict(
+            raw_str='```json{"a": 1, "b": "zx", "c": false}```',
+            index_to_choose=-1,
+            json_loader_name='json-repair',
+            schema_pydantic_base_model=None,
+            schema_check_type='dict',
+        ),
+        {'a': 1, 'b': 'zx', 'c': False},
+    ),
+    (
+        dict(
+            raw_str='```json{"a": 1, "b": "zx", "c": false}```',
+            index_to_choose=-1,
+            json_loader_name='json-repair',
+            schema_pydantic_base_model=_TestDictDataClass,
+            schema_check_type='dict',
+        ),
+        {'a': 1, 'b': 'zx', 'c': False},
+    ),
+    # list-cases
+    (
+        dict(
+            raw_str='```json["a", "b", "c"]```',
+            index_to_choose=-1,
+            json_loader_name='json-repair',
+            schema_pydantic_base_model=_TestListDataClass,
+            schema_check_type='list',
+        ),
+        ['a', 'b', 'c'],
+    ),
+]
+
