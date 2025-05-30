e572193dfabf7d677ec6262b12a60441841147dd
yu <yul1024@outlook.com>
2025-05-30 19:18:42 +0800

    Update test_base_agent.py

--- tests/test_langchain_toolkit/test_agent/test_base_agent.py
+++ tests/test_langchain_toolkit/test_agent/test_base_agent.py
@@ -3,7 +3,15 @@
 """
 
 from __future__ import annotations
+import pytest
+
 from langchain_toolkit.agents.base_agent import BaseAgent
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+class TestBaseAgent:
+    def test_base_agent(self):
+        ...
+
