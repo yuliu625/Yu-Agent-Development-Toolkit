68bbe02665cb39b123de11e186e3ce178d12c8da
yu <yul1024@outlook.com>
2025-05-30 19:18:44 +0800

    Update test_json_output_extractor.py

--- tests/test_agnostic_utils/test_json_output_extractor.py
+++ tests/test_agnostic_utils/test_json_output_extractor.py
@@ -6,36 +6,12 @@
 
 import pytest
 
+from tests.data.json_output_extractor_cases import JSON_OUTPUT_EXTRACTOR_CASES
 from agnostic_utils.json_output_extractor import JsonOutputExtractor
-from pydantic import BaseModel, Field
-
-
-class _TestListDataClass(BaseModel):
-    items: list[str] = Field(...)
-
-
-class _TestDictDataClass(BaseModel):
-    a: int
-    b: str
-    c: bool
-
-
-_test_extract_json_from_str_cases = [
-    (
-        dict(
-            raw_str='```json{"a": 1, "b": 2}```',
-            index_to_choose=-1,
-            json_loader_name='json-repair',
-            schema_pydantic_base_model=None,
-            schema_check_type='dict',
-        ),
-        {'a': 1, 'b': 2},
-    ),
-]
 
 
 class TestJsonOutputExtractor:
-    @pytest.mark.parametrize('inputs, expected', _test_extract_json_from_str_cases)
+    @pytest.mark.parametrize('inputs, expected', JSON_OUTPUT_EXTRACTOR_CASES)
     def test_extract_json_from_str(self, inputs, expected):
         json_data = JsonOutputExtractor.extract_json_from_str(**inputs)
         assert json_data == expected
