18341007e2db5a79aeccdbf0f26c277f5fe5b918
yu <yul1024@outlook.com>
2025-05-30 10:11:14 +0800

    Update cached_llm_factory.py

--- langchain_toolkit/model_factory/cached_llm_factory.py
+++ langchain_toolkit/model_factory/cached_llm_factory.py
@@ -4,9 +4,9 @@
 对于创建client需要的资源很小，多个model-client很有利于并行加速和资源管理，因此这个factory很少使用。
 """
 
+from __future__ import annotations
 
 from langchain_openai import ChatOpenAI
-
 import os
 from dotenv import load_dotenv
 
