3d73c5aa73fca21c12acd0e21c07170f1d3ce3c6
yu <yul1024@outlook.com>
2025-05-30 15:16:00 +0800

    Create test_vlms.py

---
+++ tests/test_langchain_toolkit/test_model_factory/test_vlms.py
@@ -0,0 +1,11 @@
+"""
+VLM的测试工程。
+"""
+
+from __future__ import annotations
+import pytest
+
+from langchain_toolkit.model_factory.vlm_factory import VLMFactory
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
