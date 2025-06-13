e1b79a9e6d2f1fb91dae17b2f4bc9640398deb61
yu <yul1024@outlook.com>
2025-06-13 20:40:35 +0800

    Create process_manager.py

---
+++ agnostic_utils/process_manager.py
@@ -0,0 +1,41 @@
+"""
+进程管理器。独立持久运行通用工具。
+
+预期场景:
+    - 定时运行任务。
+    - 运行可能会中断的实验，需要重启。
+"""
+
+from __future__ import annotations
+
+import subprocess
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class ProcessManager:
+    @staticmethod
+    def run_a_process(
+
+    ):
+        ...
+
+    @staticmethod
+    def run_a_process_with_restart(
+
+    ):
+        ...
+
+    @staticmethod
+    def run_processes(
+
+    ):
+        ...
+
+    @staticmethod
+    def batch_run_process(
+
+    ):
+        ...
+
