e66afcf561c7839ee1d4adc0eda0c1946e1eaca2
yu <yul1024@outlook.com>
2025-06-17 14:23:42 +0800

    Update vlm_factory.py

--- langchain_toolkit/model_factory/vlm_factory.py
+++ langchain_toolkit/model_factory/vlm_factory.py
@@ -14,7 +14,7 @@ from typing import Annotated
 class VLMFactory:
     def __init__(
         self,
-        dotenv_path: str = None
+        dotenv_path: str = None,
     ):
         """
         不需要参数实现实例化的工厂。
@@ -23,14 +23,14 @@ class VLMFactory:
         这些初始化操作可以更大的项目中删除并自行管理环境变量的导入。
 
         Args:
-            dotenv_path: (str), .env文件的路径。不指定会自行寻找。
+            dotenv_path (str): .env文件的路径。不指定会自行寻找。
         """
         load_dotenv(dotenv_path=dotenv_path, override=True)
 
     @staticmethod
     def get_dashscope_vlm(
         model_name: Annotated[str, "qwen系列模型的名字"] = 'qwen-vl-max-latest',
-        **kwargs,
+        model_configs: dict = None,
     ):
         """
         获得qwen的VLM。
@@ -43,7 +43,7 @@ class VLMFactory:
             model_name=model_name,
             api_key=os.environ['DASHSCOPE_API_KEY'],
             vl_high_resolution_images=True,  # 因为文档的缺失，不是很确定这个参数是否有效。为了冗余，会在每次请求时额外再指定。
-            **kwargs,
+            **(model_configs or {}),
         )
         return dashscope_multi_modal_llm
 
