fdcc7902d81b51b6aa9e7000d824823fb29d6307
yu <yul1024@outlook.com>
2025-06-24 16:54:02 +0800

    Update test_vlms.py

--- tests/test_langchain_toolkit/test_model_factory/test_vlms.py
+++ tests/test_langchain_toolkit/test_model_factory/test_vlms.py
@@ -9,3 +9,12 @@ from langchain_toolkit.model_factory.vlm_factory import VLMFactory
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+class TestBaseVLMs:
+    """
+    暂未实现的主要原因:
+        - 在llama-index，一些单模态和多模态的模型是有区别的。
+            但是，在langchain中，单模态和多模态通常统一被chat-model支持。
+    """
+
