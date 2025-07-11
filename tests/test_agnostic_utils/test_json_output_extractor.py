29fd1be20d11521f52ae4098019d9f4e6206caf1
yu <yul1024@outlook.com>
2025-07-11 15:48:49 +0800

    Update test_json_output_extractor.py

--- tests/test_agnostic_utils/test_json_output_extractor.py
+++ tests/test_agnostic_utils/test_json_output_extractor.py
@@ -4,10 +4,14 @@
 这是一个重要的工具类，未来任需要额外扩展。
 """
 
+from __future__ import annotations
 import pytest
 
 from tests.data.json_output_extractor_cases import JSON_OUTPUT_EXTRACTOR_CASES
 from agnostic_utils.json_output_extractor import JsonOutputExtractor
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
 
 
 class TestJsonOutputExtractor:
