8dd93dcce985882b9edd4a672319720eb908062d
yu <yul1024@outlook.com>
2025-07-12 19:53:54 +0800

    Update structured_output_helper.py

--- langchain_toolkit/agents/structured_output_helper.py
+++ langchain_toolkit/agents/structured_output_helper.py
@@ -8,10 +8,35 @@
 让另一个LLM总结并进行结构化输出:
     - 异构agent，更高的灵活性。
     - 仅一条文本，价格低，任务简单。
-    - 但是，可能存在不是原始agent的本意的现象。
+    - 但是，提取的结果可能不是原始agent的本意。
 """
 
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-# if TYPE_CHECKING:
+if TYPE_CHECKING:
+    from langchain_core.language_models import BaseChatModel
+    from pydantic import BaseModel
+
+
+class StructuredOutputHelper:
+    def __init__(
+        self,
+        llm: BaseChatModel,
+        schema_pydantic_base_model: type[BaseModel],
+    ):
+        self._llm = llm
+        self._schema_pydantic_base_model = schema_pydantic_base_model
+
+    # ====主要方法。====
+    def extract_structured_data(
+        self,
+    ):
+        ...
+
+    # ====主要方法。====
+    def fix_structured_data(
+        self,
+    ):
+        ...
+
