1223fd7f8af9fd16481a19decdf1238a7d639f84
yu <yul1024@outlook.com>
2025-12-10 12:38:43 +0800

    Update structured_output_helper.py

--- langchain_toolkit/agents/structured_output_helper.py
+++ langchain_toolkit/agents/structured_output_helper.py
@@ -13,30 +13,96 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from langchain_core.messages import SystemMessage, HumanMessage
+
+from typing import TYPE_CHECKING, cast
 if TYPE_CHECKING:
     from langchain_core.language_models import BaseChatModel
     from pydantic import BaseModel
 
 
 class StructuredOutputHelper:
+    """
+    从文本中提取结构化数据的方法。
+    """
     def __init__(
         self,
         llm: BaseChatModel,
         schema_pydantic_base_model: type[BaseModel],
+        system_message: SystemMessage,
+        max_retries: int = 3,
     ):
-        self._llm = llm
-        self._schema_pydantic_base_model = schema_pydantic_base_model
+        """
+        构造结构化输出提取工具的必要参数。
 
-    # ====主要方法。====
-    def extract_structured_data(
-        self,
-    ):
-        ...
+        Args:
+            llm (BaseChatModel): 基础的用于推理的基座模型。需要具有结构化提取功能。
+            schema_pydantic_base_model (BaseModel): 基于pydantic定义的schema。
+            system_message (SystemMessage): 提取指令。在这个实现中，它不与llm绑定，可进行修改。
+            max_retries (int): 最大重试次数。基于runnable本身的实现。
+        """
+        self._system_message = system_message
 
-    # ====主要方法。====
-    def fix_structured_data(
+        self._structured_llm = self._build_structured_llm(
+            llm=llm,
+            schema_pydantic_base_model=schema_pydantic_base_model,
+            max_retries=max_retries,
+        )
+
+    # ====暴露方法。====
+    async def extract_structured_data(
         self,
-    ):
-        ...
+        raw_str: str,
+    ) -> BaseModel:
+        """
+        提取结构化数据的方法。
+
+        这个方法默认:
+            - 文本数据。目前仅对于文本进行提取。
+
+        Args:
+            raw_str (str): 原始被提取的文本。
+
+        Returns:
+            BaseModel: 基于pydantic定义的schema的数据对象。
+                调用该方法的函数，可以进一步确认提取的schema的定义。
+        """
+        response = await self._structured_llm.ainvoke(
+            input=[
+                self._system_message,
+                HumanMessage(content=raw_str),
+            ]
+        )
+        return response
+
+    # ====内部构建方法。====
+    def _build_structured_llm(
+        self,
+        llm: BaseChatModel,
+        schema_pydantic_base_model: type[BaseModel],
+        # system_message: SystemMessage,
+        max_retries: int = 3,
+    ) -> BaseChatModel:
+        """
+        构造structured_llm的方法。
+
+        这个实现默认:
+            - 不对于system-message进行限制，不将system-message与llm提前绑定。
+                - 但相关控制需要使用structured_llm的方法实现。
+
+        Args:
+            llm (BaseChatModel): 基础的用于推理的基座模型。需要具有结构化提取功能。
+            schema_pydantic_base_model (BaseModel): 基于pydantic定义的schema。
+            max_retries (int): 最大重试次数。基于runnable本身的实现。
+
+        Returns:
+            BaseChatModel: 被限制为仅会进行结构化输出的structured_llm。
+        """
+        structured_llm = llm.with_structured_output(
+            schema=schema_pydantic_base_model,
+        ).with_retry(
+            stop_after_attempt=max_retries,
+        )
+        structured_llm = cast('BaseChatModel', structured_llm)
+        return structured_llm
 
