c6cf8c9cd08471c0e2864e32b33cb76601a9a7f7
yu <yul1024@outlook.com>
2026-01-19 17:34:07 +0800

    test: add tests

--- tests/unit_tests/test_langchain_toolkit/test_agent/test_base_agent_v2.py
+++ tests/unit_tests/test_langchain_toolkit/test_agent/test_base_agent_v2.py
@@ -18,7 +18,8 @@ from langchain_core.messages import (
 from pydantic import BaseModel, Field
 
 from typing import TYPE_CHECKING
-# if TYPE_CHECKING:
+if TYPE_CHECKING:
+    from langchain_core.language_models import BaseChatModel
 
 
 class Person(BaseModel):
@@ -27,15 +28,38 @@ class Person(BaseModel):
 
 
 class TestBaseAgent:
-    @pytest.mark.parametrize()
+    @pytest.mark.parametrize(
+        "main_llm, main_llm_system_message", [
+        (LocalLLMFactory.create_ollama_llm(model_name='qwen2.5:1.5b', reasoning=None, temperature=0.7, num_predict=None, model_configs={}),
+        SystemMessage(content="You are Bob.")),
+    ])
+    @pytest.mark.asyncio
     async def test_normal_agent(
         self,
+        main_llm: BaseChatModel,
+        main_llm_system_message: SystemMessage,
     ):
-        ...
+        agent = BaseAgent(
+            main_llm=main_llm,
+            main_llm_system_message=main_llm_system_message,
+            main_llm_max_retries=3,
+            is_need_structured_output=False,
+            formatter_llm=None,
+            schema_pydantic_base_model=None,
+            formatter_llm_system_message=None,
+            formatter_llm_max_retries=3,
+        )
+        response = await agent.a_call_llm_with_retry(
+            messages=[
+                main_llm_system_message,
+                HumanMessage("Who are you?"),
+            ],
+        )
+        logger.info(f"\nLLM Response: \n{response}")
 
-    @pytest.mark.parametrize()
-    async def test_structured_output_agent(
-        self,
-    ):
-        ...
+    # @pytest.mark.parametrize()
+    # async def test_structured_output_agent(
+    #     self,
+    # ):
+    #     ...
 
