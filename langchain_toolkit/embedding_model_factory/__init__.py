7b2d41d9c7579aaa4207edcdd7324db580d6022a
yu <yul1024@outlook.com>
2026-01-19 16:50:31 +0800

    docs: update docstring

--- langchain_toolkit/embedding_model_factory/__init__.py
+++ langchain_toolkit/embedding_model_factory/__init__.py
@@ -1,6 +1,7 @@
 """
 embedding-model-factory。
 
-这部分已经独立构建新的repository，RAG相关的工具请查看: https://github.com/yuliu625/Yu-RAG-Toolkit 。
+这部分是和RAG相关的:
+    该内容已独立构建新的repository，RAG相关的工具请查看: https://github.com/yuliu625/Yu-RAG-Toolkit 。
 """
 
