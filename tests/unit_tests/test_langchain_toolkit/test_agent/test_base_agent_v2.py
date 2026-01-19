61dfec7fc1250ddac3dc0291c759d90d884412be
yu <yul1024@outlook.com>
2026-01-19 17:00:33 +0800

    test: add tests

---
+++ tests/unit_tests/test_langchain_toolkit/test_agent/test_base_agent_v2.py
@@ -0,0 +1,17 @@
+"""
+测试BaseAgent v2的情况。
+"""
+
+from __future__ import annotations
+import pytest
+from loguru import logger
+
+from langchain_toolkit.agents.base_agent_v2 import BaseAgent
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class TestBaseAgent:
+    ...
+
