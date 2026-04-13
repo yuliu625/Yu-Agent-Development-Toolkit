51566dcfef68f809e2a30a81271250bb716f0c74
yu <yul1024@outlook.com>
2026-04-13 15:35:06 +0800

    feat: discard embedding model factory

---
+++ _old_or_discarded/embedding_model_factory/__init__.py
@@ -0,0 +1,8 @@
+"""
+embedding-model-factory
+
+Notes:
+    这部分和 RAG 相关:
+        该内容已独立构建新的 repository，RAG 相关的工具请查看: https://github.com/yuliu625/Yu-RAG-Toolkit 。
+"""
+
--- langchain_toolkit/embedding_model_factory/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-embedding-model-factory。
-
-这部分是和RAG相关的:
-    该内容已独立构建新的repository，RAG相关的工具请查看: https://github.com/yuliu625/Yu-RAG-Toolkit 。
-"""
-
