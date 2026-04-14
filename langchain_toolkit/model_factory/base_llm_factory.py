da7b877a91eca054161e097dd255a9049e2f7d45
yu <yul1024@outlook.com>
2026-04-14 11:22:09 +0800

    feat: add base llm factory

--- langchain_toolkit/model_factory/base_llm_factory.py
+++ langchain_toolkit/model_factory/base_llm_factory.py
@@ -1,37 +1,45 @@
 """
-Source: https://github.com/yuliu625/Yu-Agent-Development-Toolkit/blob/main/langchain_toolkit/model_factory/base_llm_factory.py
+Sources:
+    https://github.com/yuliu625/Yu-Agent-Development-Toolkit/langchain_toolkit/model_factory/base_llm_factory.py
 
-生成LLM的工厂。
+References:
+    None
 
-以openai兼容API实现的LLM-factory。
-可以基于这个factory进行扩展，以指定更加具体的模型，例如完全无参数获取LLM。
+Synopsis:
+    生成LLM的工厂。
 
-仅需:
-```shell
-pip install -U langchain-openai
-```
+Notes:
+    以 openai 兼容 API 实现的 LLM-factory 。
+    可以基于这个 factory 进行扩展，以指定更加具体的模型，例如完全无参数获取 LLM 。
 
-如果需要:
-    - 使用具体厂商的一些功能: 使用SpecificLLMFactory。
-    - 使用本地模型: 使用SpecificLLMFactory，以及参考LocalLLMFactory具体去实现。
+    仅需:
+    ```shell
+    pip install -U langchain-openai
+    ```
 
-未来改动: 由于科研导向需求，该工具未来做出以下改动:
-    - 中间件: 自行维护各种API过于费力，未来考虑使用如LiteLLM，仅维护配置文件，不再进行具体实现。
-    - 中间商: 考虑支付部分比例服务器，使用中间商统一服务。
+    如果需要:
+        - 使用具体厂商的一些功能: 使用 SpecificLLMFactory 。
+        - 使用本地模型: 使用 SpecificLLMFactory ，以及参考 LocalLLMFactory 具体去实现。
+
+    未来改动: 由于科研导向需求，该工具未来做出以下改动:
+        - 中间件: 自行维护各种 API 过于费力，未来考虑使用如 LiteLLM ，仅维护配置文件，不再进行具体实现。
+        - 中间商: 考虑支付部分比例服务器，使用中间商统一服务。
 """
 
 from __future__ import annotations
+from loguru import logger
 
 from langchain_openai import ChatOpenAI
 import os
 from dotenv import load_dotenv
 
-from typing import Literal
+from typing import TYPE_CHECKING, Literal
+# if TYPE_CHECKING:
 
 
 class BaseLLMFactory:
     """
-    默认的全部以openai兼容API实现的LLM-factory。
+    默认的全部以 openai 兼容 API 实现的 LLM-factory 。
     """
     # ====主要方法。====
     @staticmethod
@@ -41,7 +49,7 @@ class BaseLLMFactory:
         model_configs: dict = None,
     ) -> ChatOpenAI:
         """
-        使用strategy-pattern封装的全部的方法。
+        使用 strategy-pattern 封装的全部的方法。
 
         复杂构造仍需要传递对象参数。
         可以直接使用该工具类中其他方法。
@@ -49,10 +57,10 @@ class BaseLLMFactory:
         Args:
             model_client (Literal['openai', 'google', 'anthropic', 'dashscope', 'deepseek']): 模型的供应商。
             model_name (str): 具体模型的型号。
-            model_configs (dict, optional): 对于ChatOpenAI构造函数指定的kwargs。
+            model_configs (dict, optional): 对于 ChatOpenAI 构造函数指定的 kwargs 。
 
         Returns:
-            ChatOpenAI: langchain中可用于对话的LLM。
+            ChatOpenAI: langchain 中可用于对话的 LLM 。
         """
         if model_client == 'openai':
             return BaseLLMFactory.create_openai_llm(model_name=model_name, model_configs=model_configs)
