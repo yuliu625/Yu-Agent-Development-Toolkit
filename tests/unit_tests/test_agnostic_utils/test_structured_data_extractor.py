8327e422e2763b3a96e02e62f2402282eb1429e3
yu <yul1024@outlook.com>
2026-06-06 15:06:55 +0800

    test: add tests

--- tests/unit_tests/test_agnostic_utils/test_structured_data_extractor.py
+++ tests/unit_tests/test_agnostic_utils/test_structured_data_extractor.py
@@ -5,10 +5,10 @@
 from __future__ import annotations
 import pytest
 
-from agnostic_utils.structured_data_extractor import StructuredDataExtractor
+from modules.content_processors.structured_data_extractor import StructuredDataExtractor
 from tests.data.structured_data_extractor_cases import STRUCTURED_DATA_EXTRACTOR_CASES
 
-from typing import TYPE_CHECKING
+
 # if TYPE_CHECKING:
 
 
